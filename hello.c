055c1cb7dec55160b3de669e074edbab6b89479b
celt <digebin@gmail.com>
2021-10-19 01:20:52 +0500

    Update example

--- hello.c
+++ hello.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <tdl.h>
 
 int
@@ -6,14 +8,24 @@ main (void)
   tdl_canvas_t *canv = tdl_canvas ();
   tdl_text_t text = tdl_default_text ("Hello world!");
 
-  /* Hard clear of your terminal :P */
-  system ("clear");
+  /* Disable cursor and enable alternate screen */
+  tdl_terminal_set_alternate_screen (true);
+  tdl_terminal_set_cursor (false);
 
-  tdl_set_cursor_pos (canv, tdl_point (6, 4));
+  /* Set the cursor to the position to print text in the middle of the screen
+   */
+  tdl_set_cursor_pos (
+      canv, tdl_point ((int)(canv->size.width - text.string->length) / 2,
+                       (int)canv->size.height / 2));
   tdl_print(canv, text);
 
   /* Display all canvas changes to terminal */
   tdl_display (canv);
+
+  getchar ();
+  
+  tdl_terminal_set_cursor (true);
+  tdl_terminal_set_alternate_screen (false);
 
   /* Freeing memory from our objects */
   tdl_destroy_canvas (canv);
