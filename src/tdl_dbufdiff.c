b644494c9e52c053c16b4dd37cc58739e2845a81
celtrecium <digebin@gmail.com>
2026-03-21 04:45:18 +0500

    add license

--- src/tdl_dbufdiff.c
+++ src/tdl_dbufdiff.c
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Text Drawing Library.
+ *
+ * Text Drawing Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Text Drawing Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Text Drawing Library. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 #include "tdl/tdl_rowdiff.h"
 #include "tdl/tdl_dbufdiff.h"
 
